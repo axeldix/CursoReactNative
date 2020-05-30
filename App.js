af7044fe6c040ccafc993266c4656fb84d405d34
axdiaz <axeldiaz89@gmail.com>
2020-05-29 21:00:50 -0300

    dos pantallas utilizando stackNavigator que navegan entre ellas

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+function ScreenUno({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text onPress={ () => navigation.navigate("Screen-Dos")}>Pantalla Uno</Text>
+    </View>
+  );
+}
+
+function ScreenDos({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text onPress={ () => navigation.navigate("Screen-Uno") }>Pantalla Dos</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen-Uno" component={ScreenUno} />
+        <Stack.Screen name="Screen-Dos" component={ScreenDos} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
